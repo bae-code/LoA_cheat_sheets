22348e6c786bb0f793eef76d47fbf71b8ced8919
seonghyeon <locing45@gmail.com>
2022-01-26 02:00:01 +0900

    add - 특수문자 필터

--- LoA_cheats/guestbook/views.py
+++ LoA_cheats/guestbook/views.py
@@ -2,6 +2,7 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from .models import GuestbookModel
+import re
 # Create your views here.
 def load_guestbook(request):
     if request.method == "GET":
@@ -15,16 +16,17 @@ def load_guestbook(request):
         badwords = badwords_list.readlines()
 
         for i in badwords: 
-            if content.find(i.strip()) >= 0:
+
+            if content.find(i.strip()) >= 0 or content.isalnum():
                 content = '금지어'
-                break 
+                break
         if content == "":
             all_comment = GuestbookModel.objects.all().order_by('-created_date')
             return render(request, 'guestbook/guestbook.html',{'error':'내용을 입력해주세요','comments': all_comment})
         else:
             if content =="금지어":
                 all_comment = GuestbookModel.objects.all().order_by('-created_date')
-                return render(request, 'guestbook/guestbook.html',{'error':'금지어를 입력하셨습니다','comments': all_comment})
+                return render(request, 'guestbook/guestbook.html',{'error':'금지어 혹은 특수문자를 입력하셨습니다','comments': all_comment})
             else:
                 author = author
                 my_comment = GuestbookModel.objects.create(author=author, content=content)
